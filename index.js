a0f608d8abc4eabb79aa0ba78f8ae194aa8a7f90
dhafer-chihi <chihidhafer@gmail.com>
2023-03-17 14:49:06 +0100

    db update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const { Sequelize } = require('sequelize')
 const app = express()
 
 const port = process.env.PORT || 3000
@@ -8,5 +8,18 @@ app.get('/' , (req , res)=>{
     const message = 'Hello node !'
     res.json({message})
 })
+
+
+const sequelize = new Sequelize('test' , 'root' , '' ,{
+    host : 'localhost',
+    dialect : 'mariadb',
+    logging : false
+})
+
+
+sequelize.authenticate()
+    .then(_=>console.log('Cennection has been established successfully.'))
+    .catch(error=>console.log('Unable to connect to the database:' , error))
+   
 
 app.listen(port ,() => console.log(`Le serveur démarre sur : http://localhost:${port}`))
