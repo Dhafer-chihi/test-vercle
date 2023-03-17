d01d5a7327c3ebeda438447f27fb2248b9c3cc04
dhafer-chihi <chihidhafer@gmail.com>
2023-03-17 12:47:38 +0100

    update

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
-
+app.get('/' , (req , res)=>{
+    const message = 'Hello node !'
+    res.json({message})
+})
 
 app.listen(port ,() => console.log(`Le serveur démarre sur : http://localhost:${port}`))
